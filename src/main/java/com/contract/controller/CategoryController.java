a64b57cb9f81c4c0b46af1b71777a27fe6174271
khanhlv5 <khanhlv5@vpbank.com.vn>
2020-01-14 14:47:17 +0700

    update

--- src/main/java/com/contract/controller/CategoryController.java
+++ src/main/java/com/contract/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.contract.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,22 @@ public class CategoryController extends AbstractController {
     @GetMapping({"/", "/list"})
     public String index(Model model) {
         return "category/list";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        return "category/form";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String add(Model model, @PathVariable(name = "id") String id) {
+        System.out.println(id);
+        return "category/form";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable(name = "id") String id) {
+        System.out.println(id);
+        return "category/form";
     }
 }
